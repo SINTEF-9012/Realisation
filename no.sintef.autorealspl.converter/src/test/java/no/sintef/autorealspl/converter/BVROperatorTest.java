cf2c468ff1bf7602b89e3e0a416e258e5bc5d111
vassik <Anatoly.Vasilevskiy@sintef.no>
2016-03-19 11:04:32 +0100

    initial commit for converter from bvr to operators and dependencies required

---
+++ no.sintef.autorealspl.converter/src/test/java/no/sintef/autorealspl/converter/BVROperatorTest.java
@@ -0,0 +1,24 @@
+package no.sintef.autorealspl.converter;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BVROperatorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
